39af7f3be85a82325412f7bdb65e22e6bba8515d
Igor Bobko <limit-speed@yandex.ru>
2016-02-27 18:09:07 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!" + " Welcome to the my programm!");
+
+
+        System.exit(0);
     }
 }
