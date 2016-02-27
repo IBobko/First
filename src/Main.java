d25bc16cb53374df0bf3fb3108922a7bc8eca982
Igor Bobko <limit-speed@yandex.ru>
2016-02-27 18:13:31 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!" + " Welcome to the my programm!");
 
+        System.out.println("We are champions!!!");
+
+
 
         System.exit(0);
     }
